372854fc24f8b8ef57d41056f055739ea00d5cf7
DPhuoc <phuoc7c@gmail.com>
2024-04-23 03:23:32 +0700

    NFT contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,31 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
-    solidity: "0.8.4",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.20",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200
+                    }
+                }
+            }
+        ]
+    },
     paths: {
         artifacts: "./src/backend/artifacts",
         sources: "./src/backend/contracts",
         cache: "./src/backend/cache",
         tests: "./src/backend/test"
     },
+    neworks: {
+        hardhat: {
+            chainId: 1337
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545"
+        }
+    }
 };
---
+++ src/backend/contracts/NFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFT is ERC721URIStorage {
+    uint256 public tokenCount;
+
+    constructor() ERC721("Dapp NFT", "DAPP") {}
+
+    // Mint a new NFT
+    function mint(string memory _tokenURI) public returns (uint256) {
+        tokenCount++;
+        uint256 tokenId = tokenCount;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        return tokenId;
+    }
+}
